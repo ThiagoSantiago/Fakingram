1cf8e99ed90d1e7b5b82eba5b7356cf56518c536
thiago-santiago_glft <thiago.santiago@greenlight.me>
2023-01-29 17:33:47 -0300

    create the project

---
+++ Fakingram/Fakingram/FakingramApp.swift
@@ -0,0 +1,17 @@
+//
+//  FakingramApp.swift
+//  Fakingram
+//
+//  Created by Thiago Santiago (Contractor) on 22/01/23.
+//
+
+import SwiftUI
+
+@main
+struct FakingramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
